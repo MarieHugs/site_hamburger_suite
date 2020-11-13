986536517a1a0589f9439f3cca4aff7b51f474de
Marie-Eve Lamontagne <marie-evelamontagne1@hotmail.com>
2020-11-13 12:00:22 -0500

    fin du javascript

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
+
     var informations = [
         {
             id: "pizza",
@@ -30,6 +31,66 @@ document.addEventListener("DOMContentLoaded", function(event) {
             gallery: ["images/donuts/1.jpg", "images/donuts/2.jpg", "images/donuts/3.jpg"]
         }
     ]
+
+    if (document.querySelector(".page-item")){
+        initItem();
+    }
+
+    function initItem(){
+
+        var URL = window.location.search; //va chercher mon URL
+        console.log(URL);
+
+        var params = new URLSearchParams(URL); // va chercher précisément les  paramètres de mon URL
+        console.log(params);
+
+        //quel type de contenu qu'on veut
+
+        var type = params.get("type") || "bacon";//cible le paramètre de mon url et remet sa valeur
+        console.log(type);
+
+
+        var contenu = informations.find(element => element.id === type); //cherche dans le tableau information tous les éléments a qui les éléments id sont égal au type de ma page
+        console.log(contenu);
+
+
+        var titre = document.querySelector(".main-title");
+        titre.innerHTML = contenu.title;
+
+        var slogan = document.querySelector(".sub-title");
+        slogan.innerHTML = contenu.slogan;
+
+        var desc = document.querySelector(".sub-description p");
+        desc.innerHTML = contenu.description;
+
+        var img = document.querySelector(".sub-image img");
+        img.src = contenu.image;
+
+
+        var liste = document.querySelector(".type-list");
+        liste.innerHTML = "";
+
+        for(let i=0; i<contenu.type.length ;i++) { //fait une boucle for sur le contenu du tableau informations pour ciblé chaque items de liste présent
+            console.log(contenu.type[i]);
+
+            let li = document.createElement("li"); //crée un élément li dans le document
+
+            li.innerHTML = contenu.type[i]; // initie le contenur qui sera afficher avec le contenu du tableau informations
+            liste.appendChild(li); //cible notre variable liste de notre page HTML et crée le contenu que l'on veut soit le li à l'endroit désiré sopit dans la liste sur la page HTML
+        }
+
+
+        var thumbnails = document.querySelectorAll(".thumb img"); // cible toutes les images dans les div thumbs identifier
+        thumbnails.src = "" //met la src des images a nul ou juste vide
+
+        for(let index = 0; index<thumbnails.length; index++){ //fait une boucle sur les images de la gallerie de notre -page HTMl
+            console.log(thumbnails[index]);
+
+            var thumbs = thumbnails[index];
+            thumbs.src = contenu.gallery[index]; //cible la src des images pour la remplacer par le contenu du tableau informations
+        }
+    }
+
 
 
 });
